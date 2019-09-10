a8bea127c497d5f8ef1ddb510f89026294c3bef7
Ahmed Dawood <55143229+Hydraxx10@users.noreply.github.com>
2019-09-11 01:57:03 +0200

    Change for the image showing or hiding the password text

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
 function showPass() {
     var x = document.getElementById('password');
+    var imgVis = document.getElementById('imgVis')
     if (x.type === 'password') {
         x.type = 'text';
+        imgVis.remove()
+        var eye = document.getElementById('eye').innerHTML = '<i id="imgVis" class="fas fa-eye-slash"></i>'
     } else {
         x.type = 'password';
-    }
+        imgVis.remove()
+        var eye = document.getElementById('eye').innerHTML = '<i id="imgVis" class="far fa-eye"></i>'
+    } return false;
 }
