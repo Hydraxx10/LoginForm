7b07611af482f011e2c35f2edeb8a1ced59d411c
Ahmed Dawood <55143229+Hydraxx10@users.noreply.github.com>
2019-09-13 15:09:30 +0200

    Check box now works when click on remember me

--- javascript.js
+++ javascript.js
@@ -1,13 +1,22 @@
 function showPass() {
     var x = document.getElementById('password');
     var imgVis = document.getElementById('imgVis')
-    if (x.type === 'password') {
+    if (x.type === 'password' && x.value !== '') {
         x.type = 'text';
         imgVis.remove()
         var eye = document.getElementById('eye').innerHTML = '<i id="imgVis" class="fas fa-eye-slash"></i>'
-    } else {
+    } else if (x.type === 'text' && x.value !== '' ){
         x.type = 'password';
         imgVis.remove()
         var eye = document.getElementById('eye').innerHTML = '<i id="imgVis" class="far fa-eye"></i>'
     } return false;
+}
+
+function showCheckMark() {
+    var checkMark = document.getElementById('checkIt').checked;
+    if (checkMark === false) {
+        document.getElementById('checkIt').checked = true;
+    } else {
+        document.getElementById('checkIt').checked = false;
+    }
 }
